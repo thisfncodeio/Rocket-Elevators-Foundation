46e7991c2b111d76245241ed0378e1698fb7cb56
Abdul Akeeb <aakibu.dev@gmail.com>
2021-04-30 09:53:16 -0400

    mimemagic

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.16.0"
 before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "AbdulAkeeb"
-set :repo_url, "https://github.com/thisfncodeio/Rocket_Elevators_API"
+set :repo_url, "https://github.com/thisfncodeio/Rocket-Elevators-Foundation"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
